1527959570945274f3a179d0e638d80f22e43e11
Sebastian Ziegler <sebastian.ziegler@dkfz-heidelberg.de>
2023-05-25 12:02:21 +0200

    bash scripts for submission

--- eval.py
+++ eval.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--pred_dir",
         type=str,
-        default="./pred5",
+        default="./pred",
     )
     parser.add_argument(
         "--gt_dir",
@@ -31,19 +31,5 @@ if __name__ == "__main__":
     print('Computing Scores...')
     results = compute_all_matches(gt_list, pred_list, load_fn, num_processes=12)
     #print('Done')
-    ID = np.array([np.array(([r[2] for r in results[i]])).mean() for i in range(len(results))]).mean()
-    print('Mean Instance Dice:', ID)
-
-    obj_level_f1_per_img = []
-    for img in range(len(results)):
-        TP = np.sum([i[0] is not None and i[1] is not None for i in results[img]])
-        FP_PLUS_FN = np.sum([i[0] is None or i[1] is None for i in results[img]])
-        obj_level_f1 = 2*TP / (2*TP + FP_PLUS_FN)
-        obj_level_f1_per_img.append(obj_level_f1)
-    
-    ObjF1 = np.array(obj_level_f1_per_img).mean()
-
-    print('Obj Level F1:', ObjF1)
-
-    print('Score:', np.mean([ID, ObjF1]))
-
+    score = np.array([np.array(([r[2] for r in results[i]])).mean() for i in range(len(results))]).mean()
+    print('Mean Instance Dice:', score)
